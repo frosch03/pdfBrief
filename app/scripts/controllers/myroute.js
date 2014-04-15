5241bc8585782cfc801f4bca2a3a855165fe6b04
Matthias Brettschneider <frosch03@gmail.com>
2014-04-16 00:19:40 +0200

    added bootstrap and changed the layout a bit

--- app/scripts/controllers/myroute.js
+++ app/scripts/controllers/myroute.js
@@ -9,5 +9,10 @@ angular.module('angularApp')
     ];
     $scope.absender = { vname: 'Kurt'
                       , nname: 'Deuter'
+                      , street: 'Schwabstraße'
+                      , nr: '34/1'
+                      , postnr: '72074'
+                      , city: 'Tübingen'
                       };
+    $scope.initialchar = $scope.absender.vname.charAt(0);
   });
