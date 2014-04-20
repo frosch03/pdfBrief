f15a04c01cd79f58f188f54fc073fc38040c5891
Matthias Brettschneider <frosch03@gmail.com>
2014-04-20 12:22:24 +0200

    added localStorage & optimized the .tex-file a little

--- app/scripts/controllers/brief.js
+++ app/scripts/controllers/brief.js
@@ -1,55 +1,56 @@
 'use strict';
 
 angular.module('angularApp')
-  .controller('BriefCtrl', function ($scope) {
-      $scope.absender = {
-        vname: 'a',
-        nname: 'b',
-        street: 'c',
-        nr: 'd',
-        postnr: 'e',
-        city: 'f'
-      };
-
-      $scope.empfaenger = {
-        vname: 'g',
-        nname: 'h',
-        street: 'i',
-        nr: 'j',
-        postnr: 'k',
-        city: 'l'
-      };
-
-      $scope.meta = {
-        abs: $scope.absender,
-        empf: $scope.empfaenger,
-        txt: 'm',
-        subject: 'n',
-        date: 'o',
-        opening: 'p',
-        closing: 'q'
-      };
-
-      $scope.panelColor = function ($scope) {
-        if ( $scope.metaForm.subject.$invalid ) {
-          console.log($scope.metaForm.subject);
-          return 'panel-warning';
-        } else {
-          console.log('fuck');
-          return 'panel-default';
-        }
-      };
-
-      // $scope.out = function ($scope) {
-      //     return $scope.meta.betreff;
+  .controller('BriefCtrl', ['$scope', 'storage', function ($scope, storage) {
+      storage.bind($scope, 'meta', { 
+          defaultValue: { 
+              abs:  {vname: '',nname: '',street: '',nr: '',postnr: '',city: ''},
+              empf: {vname: '',nname: '',street: '',nr: '',postnr: '',city: ''},
+              txt:     '',
+              subject: '',
+              date:    '',
+              opening: 'Sehr geehrte Frau',
+              closing: 'Mit freundlichen Grüßen'
+          }
+      });
+      
+      // $scope.absender = {
+      //   vname: 'a',
+      //   nname: 'b',
+      //   street: 'c',
+      //   nr: 'd',
+      //   postnr: 'e',
+      //   city: 'f'
       // };
 
-      // $scope.out.changeEvent(functon
-
-      // $scope.updateOut = function(){
-      //     $scope.out = $scope.absender.vname;
+      // $scope.empfaenger = {
+      //   vname:  '',
+      //   nname:  '',
+      //   street: '',
+      //   nr:     '',
+      //   postnr: '',
+      //   city:   ''
       // };
 
+      // $scope.meta = {
+      //   abs: $scope.absender,
+      //   empf: $scope.empfaenger,
+      //   txt:     '',
+      //   subject: '',
+      //   date:    '',
+      //   opening: 'Sehr geehrte Dame',
+      //   closing: 'Mit freundlichen Grüßen'
+      // };
+
+      // $scope.panelColor = function ($scope) {
+      //   if ( $scope.metaForm.subject.$invalid ) {
+      //     console.log($scope.metaForm.subject);
+      //     return 'panel-warning';
+      //   } else {
+      //     console.log('fuck');
+      //     return 'panel-default';
+      //   }
+      // };
 
       $scope.header =
           '\\documentclass[fontsize=11pt]{scrlttr2}\n' +
@@ -101,27 +102,6 @@ angular.module('angularApp')
           '% v\n' +
           '\n';
 
-
-          // '\\newcommand{\\dieAdresse}{' + 
-          //     $scope.absender.vname + ' ' + 
-          //     $scope.absender.nname + '\\\\' + 
-          //     $scope.absender.street + ' ' + 
-          //     $scope.absender.nr + '\\\\' + 
-          //     $scope.absender.postnr + ' ' + 
-          //     $scope.absender.city + '}\n' +
-          // '\\newcommand{\\dieAnrede}{\\Anrede{Herr}{Wurst}}\n' +
-          // '\\newcommand{\\derBetreff}{Beschwerde}\n' +
-          // '\\setkomavar{fromname}\n' +
-          // '  {\\Large{\\textsc{Kurt Deuter}}}\n' +
-          // '\\setkomavar{fromaddress}\n' +
-          // '  {Diestraße 31\\\\\n' +
-          // '   72070 Tübingen}\n' +
-          // '\\setkomavar{backaddress}\n' +
-          // '  {K.Deuter, Diestraße 31, 72070 Tübingen}\n' +
-          // '\\setkomavar{subject}\n' +
-          // '  {\\derBetreff}\n' +
-          // '\\setkomavar{fromemail}{deuter@kurt.de}\n' +
-
       $scope.body =
           '\n' +
           '% ^\n' +
@@ -141,23 +121,6 @@ angular.module('angularApp')
           '% |-- Script input\n' +
           '% v\n';
 
-
-          // '\\begin{letter}{\\dieAdresse}\n' +
-          // '\\opening{\\dieAnrede}\n' +
-          // '\n' +
-          // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis ac nulla id ultricies. Ut faucibus malesuada magna, eget\n' +
-          // 'dictum mi volutpat nec. Aenean convallis, mi porta vestibulum venenatis, risus nisl fringilla erat, vel placerat quam erat sit\n' +
-          // 'amet quam.\n' +
-          // '\n' +
-          // 'Praesent diam ligula, dapibus in nisl ac, suscipit varius velit. Nam libero libero, placerat ac sapien ut, pellentesque\n' +
-          // 'laoreet mi. Mauris pellentesque volutpat mi sed luctus. Phasellus lacinia, nisl at luctus pretium, nunc mi commodo lacus, et\n' +
-          // 'consequat turpis tortor vel augue.\n' +
-          // '\n' +
-          // 'Curabitur blandit risus ut risus lobortis, eu interdum dolor aliquet. Vestibulum mattis augue\n' +
-          // 'vitae posuere lacinia. Vestibulum mauris justo, tempor sed diam et, molestie sagittis orci.\n' +
-          // '\n' +
-          // '\\closing{Mit freundlichen Grüßen}\n' +
-
       $scope.footer =
           '% ^\n' +
           '% |-- Script input\n' +
@@ -166,66 +129,4 @@ angular.module('angularApp')
           '\\end{document}          \n' +
           '\n';
 
-
-      // $Scope.onSuccess = function (response) {
-      //     console.log('hello');
-      //     console.log(response.data);
-      // };
-
-      // $http({
-      //     method: 'POST',
-      //     url: 'http://localhost:8080/upload'
-      //     }).then(console.log('yes'));
-          
-
-      // $scope.upl = function ($scope) {
-      //     $http({
-      //         url: 'http://localhost:8080/upload',
-      //         method: 'POST',
-      //         data: $scope.absender,
-      //         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-      //     }).success(
-      //         console.log('yes men')
-      //     ).error(console.log('no no no'));
-      // };
-
-      // $scope.doUpload = function () {
-      //     console.log('vorPost');
-
-      //     upload({
-      //         url: 'http://localhost:8080/upload',
-      //         method: 'POST',
-      //         data: {
-      //             absender: $scope.absender,
-      //             empfaenger: $scope.empfaenger
-      //         }
-      //     }).then(
-      //         function (response) {
-      //             console.log(response.data); // will output whatever you choose to return from the server on a successful upload
-      //         },
-      //         function (response) {
-      //             console.error(response); //  Will return if status code is above 200 and lower than 300, same as $http
-      //         }
-      //     );
-      //     console.log('nachPost');
-      // };
-
-      //     $scope.doUpload = function () {
-      //         upload({
-      //             url: 'http://localhost:8080/upload',
-      //             data: $scope.absender
-      // // {
-      // //                 // anint: 123,
-      // //                 // aBlob: Blob([1,2,3]), // Only works in newer browsers
-      // //                 // aFile: $scope.myFile // a jqLite type="file" element, upload() will extract all the files from the input and put them into the FormData object before sending.
-      // //             }
-      //         }).then(
-      //             function (response) {
-      //                 console.log(response.data); // will output whatever you choose to return from the server on a successful upload
-      //             },
-      //             function (response) {
-      //                 console.error(response); //  Will return if status code is above 200 and lower than 300, same as $http
-      //             }
-      //         );
-      //     };
-    });
+    }]);
